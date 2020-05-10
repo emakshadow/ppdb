e3f39717463eb930299cbb328ce82212e06cab0a
Luthfi Rahman <luthfirrahman696@gmail.com>
2020-05-11 06:29:24 +0700

    'edit halaman depan => application/views/layout/masterhead.php'

--- application/views/frontend/layout/masterhead.php
+++ application/views/frontend/layout/masterhead.php
@@ -4,7 +4,7 @@
             <div class="container">
             	<img style="width: 100px; margin-top: -11%; "src="<?php echo base_url('assets/frontend/assets/img/logo-man2.png')?> ">
                 <div class="masthead-subheading">Selamat Datang</div>
-                <div class="masthead-heading text-uppercase">PPDB MAN 2 Bandung</div>
+                <div class="masthead-heading text-uppercase">PPDB MAN 2 KOTA Bandung</div>
                 <a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#services">Pendaftaran</a>
             </div>
         </header>
